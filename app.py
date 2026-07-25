f477db8012b99315a91ceea6464d866a0f6fcb5a
Kris <lokiepandak@gmail.com>
2026-07-25 09:58:11 +0800

    added /contact to return email and linkedin account

--- app.py
+++ app.py
@@ -14,5 +14,9 @@ def health():
 def about():
     return jsonify({"name": "Rayna Lyn Rupita", "role": "Backend Intern"})
 
+@app.route("/contact")
+def contact():
+    return jsonify({"email": "r5@gmail.com", "linkedIn": "Rayna Lyn A. Rupita"})
+
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
